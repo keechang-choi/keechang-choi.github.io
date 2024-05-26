21d65446f239fb95de7e736faa8d6b5d873e2b1f
keechang-choi <keechang-choi@users.noreply.github.com>
2024-05-26 12:48:17 +0000

    jekyll build from Action 523837fd2395ead2b9497d526ddef86a86e10538

---
+++ assets/js/search-data.js
@@ -0,0 +1,106 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll"],
+        "tags": [],
+        "url": "https://keechang-choi.github.io/jekyll/welcome-to-jekyll/"
+      },{
+        "title": "첫 포스팅",
+        "excerpt":"MathJax is a simple, yet powerful, way ofincluding Tex/LaTex/MathML based mathematics in HTML webpages. Usage $$\\frac{1}{n^{2}}$$inline \\(\\frac{1}{n^{2}}\\)inline \\\\(\\frac{1}{n^{2}}\\\\)inline \\(\\frac{1}{n^{2}}\\)inline \\\\[\\frac{1}{n^{2}}\\\\]block \\[\\frac{1}{n^{2}}\\]block   TODO          mathjax 확인      댓글 추가      ","categories": ["etc","math"],
+        "tags": [],
+        "url": "https://keechang-choi.github.io/etc/math/first-post/"
+      },{
+        "title": "블로그 기록 시작",
+        "excerpt":"블로그 요즘 퇴사하고 나서 이직준비라는 명목하에 개인 공부도 하고, 취미 생활도 하고 있다. 나름 널널하면서도 정신적으로는 압박이 조금 있는 상태인데, 예전부터 블로그에 일상 생활들을 기록도 할겸 공유했을때 도움이 될만한 내용이 있다면 한번 남기고 싶다는 생각이 있었어서 시작하게 됐다. 이렇게라도 여유가 생겼을때 안하면 평생 안할 것 같다. jekyll이라는 블로그 플랫폼과 github에서...","categories": ["etc","study","hobby"],
+        "tags": ["cs","graphics","ai","ps","math","exercise","drawing","game","cooking"],
+        "url": "https://keechang-choi.github.io/etc/study/hobby/blog-start/"
+      },{
+        "title": "Drawing: Toledo",
+        "excerpt":"image: /images/20210802_184101.jpg여행갔던 도시의 야경을 사진 찍어뒀다가, 나중에 펜으로 그렸던 그림. ","categories": ["hobby"],
+        "tags": ["drawing"],
+        "url": "https://keechang-choi.github.io/hobby/drawing-toledo/"
+      },{
+        "title": "Drawing: 늑대",
+        "excerpt":"태블릿으로 늑대 사진을 보고 그린 그림. krita app을 사용했다. 그리는 방법은 다음 영상들을 참고했다. https://www.youtube.com/watch?v=XWUGjgSCtxEhttps://www.youtube.com/watch?v=u5rtxBu6QNw ","categories": ["hobby"],
+        "tags": ["drawing"],
+        "url": "https://keechang-choi.github.io/hobby/drawing-wolf/"
+      },{
+        "title": "Problem Solving: Can I win? & Game theory",
+        "excerpt":"100-game 최근 코딩테스트 준비 겸 leetcode 문제를 풀다가 재밌는 문제를 발견했다.이전에 수강했던 algorithmic game theory 라는 과목에서 배웠던 내용과 겹치는 것들 도 좀 있고, 적어두면 재밌을 것 같아 정리하기로 했다. 문제의 원본은 다음과 같다. https://leetcode.com/problems/can-i-win/Can I win? 이란 문제에 나오는 100 game에 대해 간략히 설면하자면 두 명의 사람이 서로 번갈아가면서...","categories": ["study"],
+        "tags": ["ps","game theory"],
+        "url": "https://keechang-choi.github.io/study/ps-study/"
+      },{
+        "title": "Drawing: 바다 거북",
+        "excerpt":"바다 거북 사진을 보고 태블릿으로 그려봤다.바다의 투명한 느낌이 그리기 어려웠다. ","categories": ["hobby"],
+        "tags": ["drawing"],
+        "url": "https://keechang-choi.github.io/hobby/drawing-sea-turtle/"
+      },{
+        "title": "Drawing: 홍따오기",
+        "excerpt":"홍따오기 (scarlet ibis) 사진을 보고 그려봤다. 이 붉은색은 홍학의 붉은색과 같은 이유라고 하는데, 먹는 게, 새우, 조개 등의 갑각류에서 오는 색소때문이라고 한다. ","categories": ["hobby"],
+        "tags": ["drawing"],
+        "url": "https://keechang-choi.github.io/hobby/drawing-scarlet-ibis/"
+      },{
+        "title": "ECS(Entity Component System)",
+        "excerpt":"Theory ECS 란 한마디로 data-oriented framework라고 할 수 있다. Vulkan API 공부를 하다가 알게됐는데, 예전에 Unity에서도 다뤄봤던 개념이었다. 그때는 뭔지 모르고 단순히 구조를 따라가서 사용하기만 했었는데 왜 이런 구조를 사용하는지 알게되니 꽤 재미있는 내용이란 생각이 들었다. 관련해서 간단하지만 원리를 잘 설명해주는 블로그 글을 보게 됐는데, 그 practice를 직접 따라 해보는...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-ECS/"
+      },{
+        "title": "Vulkan Tutorial - Texture",
+        "excerpt":"Resource Vulkan Texture mapping Images Image View and Sampler Combined Image Sampler 마무리Resource 공부한 내용들이라 내가 새로 만든 내용은 없다고 볼 수 있다. 먼저 어떤 것들을 보고 학습했는지 열거해놨으니 미리 열어보면 글에 빠진 정보를 모두 얻을 수 있다. code implementation https://github.com/keechang-choi/Vulkan-Game-Engine-Tutorial base (tutorial-27) Vulkan Game Engine Tutorial tutorial https://vulkan-tutorial.com/Texture_mapping/Images model...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-Vulkan-texture/"
+      },{
+        "title": "Vulkan Tutorial - Depth buffering & Loding models",
+        "excerpt":"이전 little vulakn game engine tutorial 을 따라오면서 대부분 이미 작성했던 내용들.가볍게 강조할 것들만 정리해놓고자 함. Depth buffering Intro 3D geometry Depth image and view Explicitly transitioning the deoth image Render pass subPass ? Frame buffer Clear values Depth and stencil state Handling window size Loading models Intro Library Sample...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-Vulkan-depth-and-model/"
+      },{
+        "title": "Vulkan Tutorial - Generating mipmaps & MultiSampling",
+        "excerpt":"Mipmap과 MultiSampling에 관한 내용도 이전 little vulakn game engine tutorial 에서 한 번 다뤘던 주제들이다. 개념들을 정리하고 넘어가려 한다. 최근에 tutorial을 끝내고 예제들을 구현해보는 단계로 넘어가면서, 내용 정리를 제대로 하지 않았다. 빠르게 넘어가서 새로운 예제들 구현에 속도를 붙이려는 생각이 있었는데, 다시 복습하는 느낌으로 정리하고 확실히 하지 않고 넘어간 부분들은 좀...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-Vulkan-mipmap-multisampling/"
+      },{
+        "title": "Vulkan Tutorial - Recap Synchronization & RenderPass",
+        "excerpt":"Vulkan Game Engine 영상에서 tutorial로 중간에 넘어오게 되면서, synchronization과 renderpass에서 가볍게 다루고 넘어간 내용들이 있어 구현에 헷갈리는 부분들이 있었다.특히 pipeline barrier와 subpass dependency 관련 검색해본 내용들을 위주로 정리하고, 관련 자료들 link를 남겨놓으려 한다.그리고 코드 구현에서 정확히 파악하고 있지 않은 부분들을 정리한 후, tutorial의 extra-chapter인 compute shader로 넘어가려 한다. Synchronization tutorial...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-Vulkan-recap-synchronization-renderpass/"
+      },{
+        "title": "Vulkan Tutorial - Compute shader",
+        "excerpt":"https://vulkan-tutorial.com/en/Compute_Shader 튜토리얼 문서에서 마지막으로 extra-chaper로 compute shader를 다루고 끝이 난다.Sascha Williems 라는 사람이 이 챕터를 작성했다고 해서, github repo를 찾아봤는데, 다양한 vulkan 예제 구현 repo가 있었다.해당 코드를 받아서 여러 예제들을 실행시켜봤는데, 다음으로 공부할 방향을 이 레포와 같게 구현해나가면 좋겠다는 생각이 들어서 내 repo도 새로 정리하고, 예제들을 하나씩 구현해나가는 형태로 계획을...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-Vulkan-compute-shader/"
+      },{
+        "title": "Vulkan Graphics Examples - Base",
+        "excerpt":"이전 글에서 적은대로, vulkan tutorial에 대한 학습을 마치고, 예제 구현 중심의 학습 방향으로 전환을 했다.들어가기 전에, 어떤 내용들을 다룰 것인지와 repo 구성 환경에 대해서 정리해 놓으려 한다.그 후, 예제 코드 구조의 기본이 되는 base 구조에 대한 작업 내용과 첫번째 예제인 triangle rendering 예제 구현까지의 내용을 다루겠다. What’s next? Cloth simulation...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-base/"
+      },{
+        "title": "Vulkan Graphics Examples - Pipelines",
+        "excerpt":"이번 예제는 pipelines 예제이다. 하나의 모델을 파일로부터 로드한 후, lighting rendering, toon rendering, wireframe rendering의 세가지를 위한 각각의 pipeline을 생성해서 화면을 분할하여 보여주는 예제이다.사실 pipeline 생성하는 작업에 대한 내용은 많지 않고, model loading을 위한 작업이 대부분을 차지했다. https://github.com/keechang-choi/Vulkan-Graphics-Example/pull/2 glTF model scene node mesh primitive material texture Loading assets flags texture...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-pipelines/"
+      },{
+        "title": "Vulkan Graphics Examples - Animation",
+        "excerpt":"이전 예제에서 glTF모델 load와 관련 구조를 작성할 때, animation과 skinning관련된 부분들은 아직 쓰이지 않아 제외하고 구현했다.이번 예제를 통해 해당 빈 부분들의 기능을 추가했다. https://github.com/keechang-choi/Vulkan-Graphics-Example/pull/3 개념 skin skinning matrix inverse bind matrix animation animation channels animation samplers data transfer structure Progress 모델 Progress-0 fix Progress-1 skinning and animation skeleton rendering instance...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-animation/"
+      },{
+        "title": "Vulkan Graphics Examples - Particles",
+        "excerpt":"compute shader 활용 예제를 base로, 이전에 tutorial에서 작성했던 것 보다 더 다양한 효과 구현에 목표를 뒀다.먼저 particle 간의 gravity simulation을 작성한 후, particle dream 예제를 구현해보는 방향을 잡았다. https://github.com/keechang-choi/Vulkan-Graphics-Example/pull/4 Motivation Prerequisites Numerical Integration Mesh Attraction Plan 작업 순서 CLI11 and ImGui Progress Synchronization Memory Barrier Particle Rendering Graphics Pipeline Particle-Calculate-Integrate...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-particles/"
+      },{
+        "title": "Vulkan Graphics Examples - PBD",
+        "excerpt":"PBD(Position Based Dynamics) 관련 내용들을 습득하고 예제들을 구현해보기로 했다. lecture에는 영상과 구현 자료가 잘 정리되어 있고 논문 및 course note 등 정확한 정보가 충분해서 공부하기 좋았다.이론과 관련되어서는 이전부터 조금씩 봐왔던 것들 중 자세히 짚고 넘어가지 못한 내용들을 정리하려 한다. numerical integration과 hash 관련 내용들이 그렇다.여러 simtulation 예시들이 나오는데, 일부는 예시들을...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-pbd/"
+      },{
+        "title": "Problem Solving: skyline",
+        "excerpt":"이전에 직장에서 computational geometry study를 가진적이 있다.그때 알게된 문제인데 skyline problem이라고 하는 유명한 문제가 leetcode에도 있어서 이 문제를 풀고 개선하려고 정리했던 적이 있다.정리해두고 잊고 있었는데, 블로그에 올려두려 한다. 문제 설명 기존 해법 개선 알고리즘 아이디어 구현 검증 및 시각화 코드 자료 10^4문제 설명 https://leetcode.com/problems/the-skyline-problem/ 먼저 skyline problem 문제 정의는 위...","categories": ["study"],
+        "tags": ["ps","computational geometry"],
+        "url": "https://keechang-choi.github.io/study/ps-skyline/"
+      },{
+        "title": "Vulkan Graphics Examples - Cloth",
+        "excerpt":"Theoretical Background Existing example implementation PBD lecture summary Lec 14 Lec 15 Lec 16 Implementation Plans Implementation items SSBO draw Constraint compute Triangle mesh \\&amp; Normal vector stretch constraint demo Atomic operations Mouse Ray casting interaction Geometry shader Results \\&amp; Further study Demo TODOs \\&amp; Not TODOs 마무리 근황 Theoretical...","categories": ["study"],
+        "tags": ["graphics"],
+        "url": "https://keechang-choi.github.io/study/study-vge-cloth/"
+      }]
